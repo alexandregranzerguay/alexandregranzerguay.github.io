f6b8baa38de081355bfe531cca5355bd3c9514c5
alexandregranzerguay <alexandre.granzerguay@gmail.com>
2016-07-30 01:33:53 -0700

    Few Edits on the plane
    MUST REVIEW AND FIX

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var main = function() {
 	/*make menu appear*/
 	$('.icon-menu span').click(function(){
-		$('.menu').animate({
-			left: "0px"
-		}, 400);
-		$(this).fadeOut(100);
+		var menuPosition = $('.menu').left;
+		if(menuPosition === "-285px"){
+			$('.menu').animate({
+				left: "0px"
+			}, 400);
+		}
+		else {
+			$('.menu').animate({
+				left: "-285px"
+			}, 400);
+		}
 	});
+	/*$('.content').click(function(){
+		$(this).animate({
+			
+		},200);
+	}); TO FIGURE OUT ONCE I HAVE INTERNET*/
 };
 
 $(document).ready(main);
