9dd387e204678833fcf3d3e9a538369c9709f53f
alexandregranzerguay <alexandre.granzerguay@gmail.com>
2016-08-10 16:51:43 -0700

    Update August 8
    Everything Fixed but more tweeks need to be done!

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 var main = function() {
 	/*make menu appear*/
 	$('.icon-menu span').click(function(){
-		var menuPosition = $('.menu').left;
-		if(menuPosition === "-285px"){
+		if($('.menu').hasClass("clsd")){
 			$('.menu').animate({
 				left: "0px"
 			}, 400);
+			$('.menu').removeClass("clsd");
+			$('.menu').addClass("opened");
 		}
 		else {
 			$('.menu').animate({
 				left: "-285px"
 			}, 400);
+			$('.menu').removeClass("opened");
+			$('.menu').addClass("clsd");
 		}
 	});
-	/*$('.content').click(function(){
-		$(this).animate({
-			
-		},200);
-	}); TO FIGURE OUT ONCE I HAVE INTERNET*/
+	$('.timeline .item').click(function(){
+		if($(this).hasClass('current')){
+			$('.item').removeClass('current');
+			$('.timeline .item .description').hide('200');
+		}
+		else{
+			$('.item').removeClass('current');
+			$('.timeline .item .description').hide('200');
+			$(this).addClass('current');
+			$(this).children('.description').show('200');
+			$('html, body').animate({
+				scrollTop: $('.timeline .item .title').offset().top
+			}, 1000);
+		}
+	});
+
 };
 
 $(document).ready(main);
