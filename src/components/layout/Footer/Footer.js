2f69c6fad7a72b5eda26b8b13bcd53b3e23d448d
Alexandre Granzer-Guay <alexandre.granzerguay@gmail.com>
2018-07-12 17:19:04 +0200

    finish v1 of front page

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -4,6 +4,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import styles from './Footer.scss';
 
 // Need this for brand icons
 library.add(fab)
@@ -11,20 +12,24 @@ library.add(fab)
 class Footer extends Component {
     render() {
         return (
-            <div>
-                <a href="https://github.com/alexandregranzerguay">
-                    <FontAwesomeIcon icon={['fab', 'github']} />
+                <div className="d-flex justify-content-center" >
+                <a href="https://github.com/alexandregranzerguay" className={styles.icon}>
+                    <FontAwesomeIcon icon={['fab', 'github']} size='4x' className={styles.git} />
                 </a>
-                <a href="https://www.linkedin.com/in/alexandre-granzer-guay-8b8378b2/">
-                    <FontAwesomeIcon icon={['fab', 'linkedin-in']} />                
+                <a href="https://www.linkedin.com/in/alexandre-granzer-guay-8b8378b2/" className={styles.icon}>
+                    <FontAwesomeIcon 
+                        icon={['fab', 'linkedin-in']} 
+                        size='4x'
+                        className={styles.linkedin}
+                    />                
                 </a>
-                <a href="https://www.facebook.com/alexandre.guay3">
-                    <FontAwesomeIcon icon={['fab', 'facebook-f']} />
+                <a href="https://www.facebook.com/alexandre.guay3" className={styles.icon}>
+                    <FontAwesomeIcon icon={['fab', 'facebook-f']} size='4x' className={styles.fb} />
                 </a>
-                <a href="https://www.instagram.com/alexandregranzerguay/">
-                    <FontAwesomeIcon icon={['fab', 'instagram']} />          
+                <a href="https://www.instagram.com/alexandregranzerguay/" className={styles.icon}>
+                    <FontAwesomeIcon icon={['fab', 'instagram']} size='4x' className={styles.insta} />          
                 </a>
-            </div>
+                </div>
         )
     }
 }
